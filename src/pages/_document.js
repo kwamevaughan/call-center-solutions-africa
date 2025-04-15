c5bb3901dd63285bb4b5e8ad75fd0dc7b0bf016d
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-04-15 14:42:02 +0000

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,11 +6,28 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Google Analytics Script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-10ELYW45R8"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-10ELYW45R8');
+              `,
+            }}
+          />
+
           {/* Add Google Fonts for Poppins */}
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+
           {/* Favicon Links */}
           <link rel="icon" href="/favicon.png" />
           <link
@@ -33,33 +50,17 @@ class MyDocument extends Document {
 
           {/* Web App Manifest for PWA */}
           <link rel="manifest" href="/manifest.json" />
-          {/* Theme color for PWA (used in the browser's address bar and splash screen) */}
           <meta name="theme-color" content="#F25849" />
-          {/* Ensure iOS recognizes the PWA */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="PAAN" />
-          {/* Link to the Apple Touch Icon */}
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-
-          {/* Google Analytics Script */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-W8K184ZV92"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-W8K184ZV92');
-              `,
-            }}
+          <meta
+            name="apple-mobile-web-app-title"
+            content="Call Center Solutions Africa"
           />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         </Head>
         <body>
           <Main />
